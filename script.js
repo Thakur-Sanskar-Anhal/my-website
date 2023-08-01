066fa69f659c5f32edbeaf1ae611a6f6ee6ed9c6
Sanskar <thakursanskaranhal@gmail.com>
2023-08-01 21:01:11 +0530

    added an accordian

---
+++ script.js
@@ -0,0 +1,21 @@
+const accordians = document.querySelectorAll('.accordian-section');
+
+accordians.forEach(accordian =>{
+    const icon = accordian.querySelector('.icon');
+    const description = accordian.querySelector('.description');
+
+    accordian.addEventListener('click',()=>{
+
+        // icon.classList.toggle('active');
+        // description.classList.toggle('active');
+
+        accordian.classList.toggle('active');
+        if (icon.classList.contains('active')) {
+            icon.classList.remove('active');
+            description.style.maxHeight = null;
+        }else{
+            icon.classList.add('active');
+            description.style.maxHeight = description.scrollHeight + 'px';
+        }
+    })
+})
